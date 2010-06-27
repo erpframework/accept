4d7a1a430420c562b06e5a3b160ec65f7211e97b
szczepiq <szczepiq@localhost>
2010-06-27 13:55:35 +0200

    Fixed the test

--- gwztests/bdd/steps/AcceptanceValidation.java
+++ gwztests/bdd/steps/AcceptanceValidation.java
@@ -5,6 +5,6 @@ import org.accept.api.RootFolder;
 import org.junit.runner.RunWith;
 
 @RunWith(AcceptRunner.class)
-@RootFolder("Accept/calculator/Iteration 12")
+@RootFolder("Accept/root/calculator/Iteration 12")
 //it is very unfortunate, but this test depends on where you run it (e.g. the work folder)
 public class AcceptanceValidation {}
